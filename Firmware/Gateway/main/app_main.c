9a3efe66265add203c01d0a3e235e6fb05cebdd2
Kyuubi <khoi.nv202647@sis.hust.edu.vn>
2024-05-23 18:39:20 +0700

    _add relay drive

--- Firmware/Gateway/main/app_main.c
+++ Firmware/Gateway/main/app_main.c
@@ -36,9 +36,14 @@
 
 
 #define LED 2
+#define RELAY1 18
+#define RELAY2 17
 static const char *TAG = "MAIN";
 char ssid[50] = {0};
 char pwd[50] = {0};
+// Pass Ezcast 0915423686
+
+
 
 //RingbufHandle_t webserver_ring_buf;
 extern esp_mqtt_client_handle_t client;
@@ -48,32 +53,46 @@ void app_main()
     esp_err_t err;
     ESP_LOGI(TAG, "Initializing...");
     err = nvs_flash_init();
+
     if (err == ESP_ERR_NVS_NO_FREE_PAGES)
     {
         ESP_ERROR_CHECK(nvs_flash_erase());
         err = nvs_flash_init();
     }
+
     ESP_ERROR_CHECK(err);
     /* chip's information*/
-    //chip_stats();
+    chip_stats();
     /* init GPIO */
     output_io_create(LED);
+    output_io_create(RELAY1);
+    output_io_create(RELAY2);
+
     /* init wifi configuration*/
     wifi_init();
     sprintf(ssid, "Kyuubi");
     sprintf(pwd, "laclac123");
     wifi_config_t wifi_config;
     bzero(&wifi_config, sizeof(wifi_config_t));
+    
     memcpy(wifi_config.sta.ssid, ssid, strlen(ssid));
     memcpy(wifi_config.sta.password, pwd, strlen(pwd));
     
     wifi_sta_start(wifi_config, WIFI_MODE_STA);
     mqtt_client_start();
-    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    // output_io_set_level(RELAY1, 1);
+    // output_io_set_level(RELAY2, 1);
+    // ESP_LOGI(TAG, "RELAY1 OFF");
+    // vTaskDelay(2000 / portTICK_PERIOD_MS);
+    // output_io_set_level(RELAY1, 0);
+    // output_io_set_level(RELAY2, 0);
+    // ESP_LOGI(TAG, "RELAY1 ON");
     while(1)
     {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
         output_io_toggle(LED);
+        
+        ESP_LOGI(TAG, "LED TOGGLE");
     }
 }
 
